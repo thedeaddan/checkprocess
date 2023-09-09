2b2d9db0e8bfa6bb2cb5e58c7819820a39c347cd
Даниил <40400854+thedeaddan@users.noreply.github.com>
2023-09-09 12:25:51 +0300

    Все переписал, все оптимизировал

--- main.py
+++ main.py
@@ -1,58 +1,74 @@
-import psi.process 
-from colorama import init, Fore
+import psi.process
+from colorama import init, Fore, Style
 import subprocess
-init(autoreset=True) #init colorama 
+from rich.traceback import install
+import shutil
 
-strandtest = False #Setting the variable to a false value
-besed = False # Setting the variable to a false value
-twitchmine = False #Setting the variable to a false value
-onlinecheck = False # Setting the variable to a false value
+install()
+init(autoreset=True)
+#TG_Bot_Max
+processes = {"TM_thedeaddan","TM_Mini", "TM_Grisha", "VK_Delete", "TG_Bot_Curs", "Server_Stat","Log_Server"}
 
-cold_temp = 38
-low_temp = 45 # Operating temperature threshold
-average_temp = 55 # Threshold of elevated temperature
-high_temp = 60 # Very high temperature threshold
+def check_temp():
+    temp = str(subprocess.check_output('vcgencmd measure_temp', shell=True)).split('"')[1].split("=")[1][:-4]
+    float_temp = int(float(temp))
 
-temp = str(subprocess.check_output('vcgencmd measure_temp', shell=True)).split('"')[1].split("=")[1][:-4] # The vcgencmd measure_temp command requests the temperature, change it to your own. Perhaps this line will need to be changed.
-float_temp = int(float(temp))
-print("[===========================]")
-if float_temp < cold_temp:
-	print("Температура = "+Fore.CYAN+temp+"°C")
-elif float_temp < low_temp: 
-	print("Температура = "+Fore.GREEN+temp+"°C")
-elif float_temp < average_temp:
-	print("Температура = "+Fore.YELLOW+temp+"°C")
-elif float_temp > high_temp:
-	print("Температура = "+Fore.RED+temp+"°C")
-print("[===========================]")
+    color_map = {
+        range(0, 38): Fore.CYAN,
+        range(38, 45): Style.BRIGHT + Fore.GREEN,
+        range(45, 55): Style.BRIGHT + Fore.YELLOW,
+        range(55, 65): Style.BRIGHT + Fore.RED,
+        range(65, 100): Fore.RED
+    }
 
-for p in psi.process.ProcessTable().values(): #Check process list
-	if "strandtest.py" in p.command: # Name of ur 1 process
-		strandtest = True
-		strandtest_pid = str(p.pid) #Get PID
-	if "besed.py" in p.command: # Name of ur 2 process
-		besed = True
-		besed_pid = str(p.pid)#Get PID
-	if "SCREEN -d -m -S Twitch_Mining" in p.command: # Name of ur 3 process
-		twitchmine = True
-		twitchmine_pid = str(p.pid)#Get PID
-	if "SCREEN -d -m -S onlinecheck" in p.command: # Name of ur 4 process
-		onlinecheck = True
-		onlinecheck_pid = str(p.pid)#Get PID
-if strandtest:
-	print("LED - Контроллер ------ "+Fore.GREEN+"OK"+" ["+strandtest_pid+"]") #if process active print OK and PID
-else:
-	print("LED - Контроллер ------ "+Fore.RED+"OШИБКА"+" [---]")# if process not active
-if besed:
-	print("Бот беседы ------------ "+Fore.GREEN+"OK"+" ["+besed_pid+"]")#if process active print OK and PID
-else:
-	print("Бот беседы ------------ "+Fore.RED+"OШИБКА"+" [---]")# if process not active
-if twitchmine:
-	print("Майнер твича ---------- "+Fore.GREEN+"OK"+" ["+twitchmine_pid+"]")#if process active print OK and PID
-else:
-	print("Майнер твича ---------- "+Fore.RED+"OШИБКА"+" [---]")# if process not active
-if onlinecheck:
-	print("Чекер онлайна --------- "+Fore.GREEN+"OK"+" ["+onlinecheck_pid+"]")#if process active print OK and PID
-else:
-	print("Чекер онлайна --------- "+Fore.RED+"OШИБКА"+" [---]")# if process not active
-print("[===========================]")
+    for temp_range, color in color_map.items():
+        if float_temp in temp_range:
+            return [color, temp]
+    return [Fore.RED, f"!!! {temp} !!!"]
+
+def generate_temp_space():
+    color = check_temp()[0]
+    space = "[" + "".join([color + "=" + Style.RESET_ALL for _ in range(35)]) + "]"
+    return space
+
+def generate_space(bot, status):
+    space_len = 20 - len(bot)
+    color = Fore.GREEN if status else Fore.RED
+    space = "".join([Style.BRIGHT + (color if i % 2 == 0 else Style.RESET_ALL) + "-" + Style.RESET_ALL for i in range(space_len)])
+    return space
+
+def print_temp():
+    print(f"Температура = {check_temp()[0]}{check_temp()[1]}°C")
+
+def check_process():
+    process_table = psi.process.ProcessTable().values()
+    for p in process_table:
+        for bot in list(processes):
+            if bot in p.command:
+                processes.remove(bot)
+                print(f"{Style.BRIGHT}{bot} {generate_space(bot, True)} {Style.BRIGHT + Fore.GREEN}Работает[{p.pid}]")
+    for bot in processes:
+        print(f"{Style.BRIGHT}{bot} {generate_space(bot, False)} {Style.BRIGHT + Fore.RED}Не работает[---]")
+
+def print_disk_usage():
+    drives = [("/", "M2"), ("/media/driveone", "HDD")]
+
+    for drive, name in drives:
+        total, used, free = shutil.disk_usage(drive)
+        total_gb = round(total / (1024**3), 2)
+        used_gb = round(used / (1024**3), 2)
+        free_gb = round(free / (1024**3), 2)
+
+        usage_percent = int((used / total) * 100)
+        usage_bar = "[" + "=" * (usage_percent // 5) + " " * ((100 - usage_percent) // 5) + "]"
+
+        print(f"{Style.BRIGHT}{name}: {usage_bar}\n{used_gb} GB/{total_gb} GB ({free_gb} GB)")
+
+temp_space = generate_temp_space()
+print(f"{temp_space}")
+print_temp()
+print(f"{temp_space}")
+check_process()
+print(f"{temp_space}")
+print_disk_usage()
+print(f"{temp_space}")
